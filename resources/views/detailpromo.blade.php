ff9eee3dc865311d36ce88cf6a1137fec7bdc5f0
JessiV20 <Jessivilla657@gmail.com>
2024-05-22 13:27:49 -0600

    imaggenes

---
+++ resources/views/detailpromo.blade.php
@@ -0,0 +1,14 @@
+@extends('partials.master')
+@include('partials.menu')
+
+@foreach($packages as $p)
+<div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{$p->name}}</h5>
+      <p class="card-text">{{$p->description}}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+  Hola
+@endforeach
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,20 +1,80 @@
 @extends('partials.master')
 @include('partials.menu')
-
-<section class="container top-categories" id="inicio">
+<section class="container top-categories1" id="inicio1">
     <h1 class="heading-1">Bienvenido</h1>
-    <div class="container-categories">
-        <div class="card-category category-moca">
-            <p>Platillos</p>
-            <a href="{{route('detail',10)}}" ><span>Ver más</span></a>
+    <div class="container-categories1">
+        <div class="card-category1 category-paquetes">
+            <p>¡Paquetes y Promos!</p>
+            <a href="{{ route('detailpromo') }}"> <span>Ver más</span></a>
         </div>
-        <div class="card-category category-expreso">
+    </div>
+</section>
+<section class="container top-categories" id="inicio">
+
+    <div class="container-categories">
+        <div class="card-category category-postres">
+            <p>Postres</p>
+            <a href="{{route('detail',3)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-cocteleria">
+            <p>Coctelería</p>
+            <a href="{{route('detail',2)}}" ><span>Ver más</span></a>
+        </div>
+        <div class="card-category category-bebidas">
             <p>Bebidas</p>
             <a href="{{route('detail',1)}}" ><span>Ver más</span></a>
         </div>
-        <div class="card-category category-capuchino">
-            <p>Postres</p>
-            <a href="{{route('detail',3)}}" > <span>Ver más</span></a>
+        <div class="card-category category-niños">
+            <p>Para Niños</p>
+            <a href="{{route('detail',4)}}" ><span>Ver más</span></a>
+        </div>
+        <div class="card-category category-caldos">
+            <p>Sopas/Caldos</p>
+            <a href="{{route('detail',5)}}" ><span>Ver más</span></a>
+        </div>
+        <div class="card-category category-entremeses">
+            <p>Entremeses</p>
+            <a href="{{route('detail',6)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-pulpo">
+            <p>Pulpo al gusto</p>
+            <a href="{{route('detail',7)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-cocteles">
+            <p>Cocteles</p>
+            <a href="{{route('detail',8)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-pescado">
+            <p>Pescado Frito</p>
+            <a href="{{route('detail',9)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-especialidades">
+            <p>Especialidades</p>
+            <a href="{{route('detail',10)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-filetes">
+            <p>Filetes</p>
+            <a href="{{route('detail',11)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-brochetas">
+            <p>Brochetas</p>
+            <a href="{{route('detail',12)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-carnes">
+            <p>Carnes/Cortes</p>
+            <a href="{{route('detail',13)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-snacks">
+            <p>Snacks</p>
+            <a href="{{route('detail',14)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-parrillada">
+            <p>Parrillada</p>
+            <a href="{{route('detail',15)}}" > <span>Ver más</span></a>
+        </div>
+        <div class="card-category category-ensaladas">
+            <p>Ensaladas</p>
+            <a href="{{route('detail',16)}}" > <span>Ver más</span></a>
         </div>
     </div>
 </section>
